3a2d314a30532619854eb8ccd77570a8281418ed
DaPeKkid <1043875@mborijnland.nl>
2021-12-06 10:21:49 +0100

    made pages, reservation.php + form. Added styles.css

---
+++ reservation.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    <title>Reserveren</title>
+</head>
+<body>
+        <section>
+            <h1>Maak uw reservering</h1>
+            <form action="" method="post">
+                Voornaam: <input type="text" id="first_name" name="voornaam" value="" ><br>
+                Achternaam: <input type="text" id="last_name" name="achternaam" value="" ><br>
+                Tel: <input type="number" id="phone_number" name="telefoonnummer" value="" ><br>
+                Email: <input type="text" id="email" name="email" value="" ><br>
+                Opmerking: <input type="text" id="comment" name="opmerkingen" value="" ><br>
+                <input type="submit" name="submit" value="send">
+            </form>
+        </section>
+</body>
+</html>
