68d4837ead6d724571bd62f2fc71d994719fdd0f
DaPeKkid <1043875@mborijnland.nl>
2021-12-06 11:23:41 +0100

    edit reservation.php and added favicon

--- reservation.php
+++ reservation.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="stylesheet" type="text/css" href="css/styles.css"/>
     <title>Reserveren</title>
 </head>
@@ -12,11 +13,12 @@
         <section>
             <h1>Maak uw reservering</h1>
             <form action="" method="post">
-                Voornaam: <input type="text" id="first_name" name="voornaam" value="" ><br>
-                Achternaam: <input type="text" id="last_name" name="achternaam" value="" ><br>
-                Tel: <input type="number" id="phone_number" name="telefoonnummer" value="" ><br>
-                Email: <input type="text" id="email" name="email" value="" ><br>
-                Opmerking: <input type="text" id="comment" name="opmerkingen" value="" ><br>
+                Voornaam: <br><input type="text" id="first_name" name="first_name" value="" ><br>
+                Achternaam: <br><input type="text" id="last_name" name="last_name" value="" ><br>
+                Tel: <br><input type="number" id="phone_number" name="phone_number" value="" ><br>
+                Email: <br><input type="text" id="email" name="email" value="" ><br>
+                Opmerkingen: <br><textarea rows="4" cols="50" name="comments">
+Plaats uw opmerking...</textarea>
                 <input type="submit" name="submit" value="send">
             </form>
         </section>
